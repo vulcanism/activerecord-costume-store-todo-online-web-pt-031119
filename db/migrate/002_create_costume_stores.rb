283898a4a63dbc3a1a3b92fcefbf1e2682580009
Savannah C <sgcorrell@gmail.com>
2019-05-30 16:09:25 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -6,7 +6,7 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
       t.string :location
       t.integer :costume_inventory
       t.integer :num_of_employees
-      t.boolean :in_business
+      t.boolean :still_in_business
       t.datetime :opening_time
       t.datetime :closing_time
     end
