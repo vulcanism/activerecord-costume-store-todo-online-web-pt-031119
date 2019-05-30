ab86775d7a2a19afe016a7eea7bc515764fac4ef
Savannah C <sgcorrell@gmail.com>
2019-05-30 16:01:28 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -1,7 +1,7 @@
 class CreateCostumes < ActiveRecord::Migration[5.2]
   
   def change
-    create table :costumes do |t|
+    create_table :costumes do |t|
       t.string :name
       t.integer :price
       t.string :size
