a73c437a326854c4e02a70f054dada33937f1cea
Savannah C <sgcorrell@gmail.com>
2019-05-30 16:05:33 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -24,22 +24,22 @@ ActiveRecord::Schema.define(version: 3) do
 
   create_table "costumes", force: :cascade do |t|
     t.string "name"
-    t.integer "price"
-    t.string "size"
+    t.float "price"
+    t.integer "size"
     t.string "image_url"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "haunted_houses", force: :cascade do |t|
     t.string "name"
     t.string "location"
     t.string "theme"
-    t.integer "price"
+    t.float "price"
     t.boolean "family_friendly"
     t.datetime "opening_date"
     t.datetime "closing_date"
-    t.text "long_description"
+    t.text "description"
   end
 
 end
