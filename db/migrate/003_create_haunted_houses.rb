d067159cec08a5e0c950ee4c0b208b7bbb5a69d5
Savannah C <sgcorrell@gmail.com>
2019-05-30 16:02:56 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -9,7 +9,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.2]
       t.boolean :family_friendly
       t.datetime :opening_date
       t.datetime :closing_date
-      t.text :long_description
+      t.text :description
     end
   end
   
