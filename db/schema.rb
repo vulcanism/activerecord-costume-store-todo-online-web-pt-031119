c2b8b857715477a650432a8974b40f87fda06d65
Savannah C <sgcorrell@gmail.com>
2019-05-30 16:10:30 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema.define(version: 3) do
     t.string "location"
     t.integer "costume_inventory"
     t.integer "num_of_employees"
-    t.boolean "in_business"
+    t.boolean "still_in_business"
     t.datetime "opening_time"
     t.datetime "closing_time"
   end
