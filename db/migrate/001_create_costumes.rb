b74a5faed3f09ebbf24f7e15a2e7cfcf2f02cbfd
Savannah C <sgcorrell@gmail.com>
2019-05-30 16:04:50 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -4,7 +4,7 @@ class CreateCostumes < ActiveRecord::Migration[5.2]
     create_table :costumes do |t|
       t.string :name
       t.float :price
-      t.string :size
+      t.integer :size
       t.string :image_url
       t.timestamp :created_at
       t.timestamp :updated_at
