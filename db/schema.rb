2776d6326ca5ea8b1e6c4b1d03943c5ce29bfd62
Savannah C <sgcorrell@gmail.com>
2019-05-30 16:03:57 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -35,11 +35,11 @@ ActiveRecord::Schema.define(version: 3) do
     t.string "name"
     t.string "location"
     t.string "theme"
-    t.integer "price"
+    t.float "price"
     t.boolean "family_friendly"
     t.datetime "opening_date"
     t.datetime "closing_date"
-    t.text "long_description"
+    t.text "description"
   end
 
 end
