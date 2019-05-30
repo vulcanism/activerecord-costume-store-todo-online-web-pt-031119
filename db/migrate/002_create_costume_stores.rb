eb1a7041e14a2c4a1e075ba11b3589fed42e6c88
Savannah C <sgcorrell@gmail.com>
2019-05-30 15:54:10 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -3,6 +3,10 @@ class CreateCostumeStores < ActiveRecord::Migration[5.2]
   def change
     create table :haunted_houses do |t|
       t.string :name
+      t.string :location
+      t.integer :costume_inventory
+      t.integer :employees
+      t.string 
     end
   end
   
